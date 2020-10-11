740a1129c369439d988e6637e7e6e05c4e72d63e
zombieman104 <daymanblake@gmail.com>
2020-10-11 14:04:43 -0700

    problems occured running application locally

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ module.exports = app;
 var path = require('path');
 
 //port will look for the heroku deploy or locally on port 9000
-port = process.env.PORT || 8080;
+// port = process.env.PORT || 8080;
 
 
-app.listen(port, function(){
-    console.log('Application is listening on 8080')
-});
+// app.listen(port, function(){
+//     console.log('Application is listening on 8080')
+// });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+  console.log('Application is running')
+}
+app.listen(port);
