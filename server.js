b508b964e24ae9e1aca2d07d9d5d16dd177030d3
zombieman104 <daymanblake@gmail.com>
2020-10-11 12:58:07 -0700

    set up portfolio to run locally and on heroku TODO: make webpage to test

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
-// sets up the static file server
+const express = require('express');
+const app = express();
+module.exports = app;
 
-// include the static file webserver library
-var static = require('node-static');
+var path = require('path');
 
-// include the http server library
-var http = require('http');
+//port will look for the heroku deploy or locally on port 9000
+port = process.env.PORT || 5000;
 
-// assume that we are running on heroku
-var port = process.env.PORT;
-var directory = __dirname + '/public';
 
-// if we arent on heroku then we need to readjust the port and directory information and we know that because the port wont be set
-if(typeof port == 'undefined' || !port){
-    directory = './public';
-    port = 8080;
-}
-
-// set up static web server that will deliver files from the filesystem
-var file = new static.Server(directory);
-
-// construct an http server that gets files from the file server
-var app = http.createServer(function(request,response){
-    request.addListener('end', 
-    function(){
-        file.serve(request,response);
-    }
-    ).resume();
-}
-).listen(port);
-console.log('The server is running');
+app.listen(port, function(){
+    console.log('Application is listening on 5000')
+});
