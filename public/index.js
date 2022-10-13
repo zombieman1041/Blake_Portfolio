1ef963373d672e87bd79655033e312477463dc33
bdayman <daymanblake@gmail.com>
2022-10-12 20:29:23 -0700

    toggle aoi and volunteer
    implemented toggle feature for the areas of interest and volunteer experience sections. Added clickable buttons to about section.

--- public/index.js
+++ public/index.js
@@ -14,4 +14,34 @@ function mobileMenu() {
       y.style.display = "none";
 
     }
+}
+function AOI_toggle() {
+  var x = document.getElementById("AOI-toggle");
+  var xx = document.getElementById("AOI");
+
+  if (x.style.display != "block"){
+    x.style.display = "block";
+    xx.style.opacity = "1";
   }
+  else{
+    x.style.display = "none";
+    xx.style.opacity = "0.6";
+  }
+
+
+}
+function volunteer_toggle() {
+
+  var y = document.getElementById("volunteer-toggle");
+  var yy = document.getElementById("volunteer");
+
+
+  if (y.style.display != "block"){
+    y.style.display = "block";
+    yy.style.opacity = "1";
+  }
+  else{
+    y.style.display = "none";
+    yy.style.opacity = "0.6";
+  }
+}
